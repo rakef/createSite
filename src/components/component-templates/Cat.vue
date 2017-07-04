c966201ec697ae2abeebb62f378b64ea455e885e
rakef <rakefet.zeiman@gmail.com>
2017-07-04 21:17:50 +0300

    added cats

---
+++ src/components/component-templates/Cat.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <a href="http://thecatapi.com">
+      <img src="http://thecatapi.com/api/images/get?format=src&type=gif">
+    </a>
+  </div>
+</template>
+<script>
+export default {
+  name: 'cat',
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  width: 100%;
+  background-color: pink;
+  padding: .2em;
+  margin: 0;
+}
+</style>
